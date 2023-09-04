fae79406d7c14f96edf70db017db9f2d615c36f4
mtarka <mtarka1337@gmail.com>
2023-09-05 00:13:43 +0200

    Cleanup

--- src/main/java/com/example/Currency/base/configuration/ApplicationConfig.java
+++ src/main/java/com/example/Currency/base/configuration/ApplicationConfig.java
@@ -1,9 +1,16 @@
 package com.example.Currency.base.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class ApplicationConfig {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
